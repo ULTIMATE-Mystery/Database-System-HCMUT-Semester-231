b634aa375188ff06f7ce90698ec10219539c4bc0
HuynPhuc <phuc.huynhphuc2k2@hcmut.edu.vn>
2023-12-07 07:27:48 +0700

    create

--- client/src/Components/Modal/CreateBillModal.js
+++ client/src/Components/Modal/CreateBillModal.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import {
-  Row,
-  Col,
   Modal,
   ModalHeader,
   ModalBody,
@@ -15,6 +13,8 @@ import {
 import axios from "axios";
 
 function formatDate(dateTimeString) {
+  
+
   const date = new Date(dateTimeString);
   date.setDate(date.getDate());
 
@@ -39,6 +39,7 @@ function formatDateTime(dateTimeString) {
 
 //main display
 function CreateOrderModal({ isOpen, toggleModal, fetchData }) {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [data, setData] = useState();
   useEffect(() => {
     fetchScheduleData(); // Gọi API khi component được render lần đầu tiên
@@ -77,6 +78,25 @@ function CreateOrderModal({ isOpen, toggleModal, fetchData }) {
       });
   };
 
+  const [testData, setTestData] = useState([]);
+  useEffect(() => {
+    fetchTestData(); // Gọi API khi component được render lần đầu tiên
+    const interval = setInterval(() => {
+      fetchTestData(); // Gọi API sau mỗi khoảng thời gian
+    }, 5000); // Thời gian cập nhật, ví dụ 5 giây
+    return () => clearInterval(interval); // Clear interval để tránh memory leak
+  }, []);
+  const fetchTestData = () => {
+    axios
+      .get("http://localhost:4000/test/data")
+      .then((response) => {
+        setTestData(response.data.data);
+      })
+      .catch((error) => {
+        console.error("Lỗi khi gọi API:", error);
+      });
+  };
+
   const currentDate = new Date();
   const currentDateString = currentDate.toISOString().split("T")[0];
   const currentTimeString = currentDate.toTimeString().split(" ")[0];
@@ -107,27 +127,103 @@ function CreateOrderModal({ isOpen, toggleModal, fetchData }) {
 
   //medicine
   const [selectedMedicines, setSelectedMedicines] = useState([]);
+  const [quantity, setQuantity] = useState(1); // Mặc định số lượng là 1
+  const [selectedMedicineName, setSelectedMedicineName] = useState("");
 
   const handleMedicineChange = (event) => {
-    const selectedMedicineName = event.target.value;
+    setSelectedMedicineName(event.target.value);
+  };
+
+  const handleQuantityChange = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  const handleAddMedicine = () => {
     const selectedMedicineInfo = medicine.find(
       (item) => item.ten_thuoc === selectedMedicineName
     );
-    if (
-      !selectedMedicines.some(
-        (item) => item.ten_thuoc === selectedMedicineInfo.ten_thuoc
-      )
-    ) {
-      setSelectedMedicines([...selectedMedicines, selectedMedicineInfo]);
+
+    const existingMedicineIndex = selectedMedicines.findIndex(
+      (item) => item.ten_thuoc === selectedMedicineName
+    );
+
+    if (existingMedicineIndex !== -1) {
+      const updatedMedicines = [...selectedMedicines];
+      updatedMedicines[existingMedicineIndex].so_luong = parseInt(quantity, 10);
+      setSelectedMedicines(updatedMedicines);
+    } else {
+      const newMedicineInfo = {
+        ...selectedMedicineInfo,
+        so_luong: parseInt(quantity, 10),
+      };
+      setSelectedMedicines([...selectedMedicines, newMedicineInfo]);
     }
+
+    setSelectedMedicineName("");
+    setQuantity(1);
   };
 
-  const removeSelectedMedicine = (index, event) => {
-    event.preventDefault();
+  const removeSelectedMedicine = (index) => {
     const updatedMedicines = [...selectedMedicines];
     updatedMedicines.splice(index, 1);
     setSelectedMedicines(updatedMedicines);
   };
+  const [selectedTests, setSelectedTests] = useState([]);
+  const [selectedTestName, setSelectedTestName] = useState("");
+  // const [testQuantity, setTestQuantity] = useState(1);
+
+  const handleTestChange = (event) => {
+    setSelectedTestName(event.target.value);
+  };
+
+  const handleAddTest = () => {
+    const selectedTestInfo = testData.find(
+      (item) => item.ten_xet_nghiem === selectedTestName
+    );
+
+    const existingTestIndex = selectedTests.findIndex(
+      (item) => item.ten_xet_nghiem === selectedTestName
+    );
+
+    if (existingTestIndex !== -1) {
+      return; // Nếu xét nghiệm đã tồn tại, không làm gì cả
+    }
+
+    const newTestInfo = {
+      ...selectedTestInfo,
+      so_luong: 1, // Số lượng mặc định là 1
+    };
+    setSelectedTests([...selectedTests, newTestInfo]);
+
+    setSelectedTestName(""); // Reset giá trị cho việc chọn xét nghiệm tiếp theo
+  };
+
+  const handleRemoveTest = (index) => {
+    const updatedTests = [...selectedTests];
+    updatedTests.splice(index, 1);
+    setSelectedTests(updatedTests);
+  };
+
+  const calculateTotalCost = () => {
+    let totalCost = 0;
+
+    // Calculate total cost for medicines
+    for (const medicine of selectedMedicines) {
+      totalCost += medicine.don_gia * medicine.so_luong * 1000;
+    }
+
+    // Calculate total cost for tests
+    for (const test of selectedTests) {
+      totalCost += test.gia_tien * test.so_luong;
+    }
+    // Set the total cost to formData
+    setFormData({ ...formData, tong_tien: totalCost });
+  };
+
+  useEffect(() => {
+    calculateTotalCost();
+  }, [selectedMedicines, selectedTests]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -138,6 +234,7 @@ function CreateOrderModal({ isOpen, toggleModal, fetchData }) {
 
   const handleSubmit = async () => {
     try {
+      setShowSuccessMessage(true);
       const formDataCopy = {
         thoi_gian_tao: formatDateTime(formData.thoi_gian_tao).toString(),
         tong_tien: parseInt(formData.tong_tien, 10),
@@ -170,7 +267,9 @@ function CreateOrderModal({ isOpen, toggleModal, fetchData }) {
       <ModalBody>
         <Form>
           <FormGroup>
-            <Label for="ma_benh_nhan">Mã Bệnh nhân</Label>
+            <Label style={{ fontWeight: "bold" }} for="ma_benh_nhan">
+              Mã Bệnh nhân
+            </Label>
             <Input
               type="number"
               name="ma_benh_nhan"
@@ -179,39 +278,98 @@ function CreateOrderModal({ isOpen, toggleModal, fetchData }) {
             />
           </FormGroup>
           <FormGroup>
-            <Label for="ten_thuoc">Thêm Thuốc</Label>
-            <Input
-              type="select"
-              name="ten_thuoc"
-              id="ten_thuoc"
-              onChange={handleMedicineChange}
-            >
-              <option value="">Chọn thuốc</option>
-              {medicine.map((item) => (
-                <option key={item.id} value={item.ten_thuoc}>
-                  {item.ten_thuoc}
-                </option>
-              ))}
-            </Input>
+            <Label style={{ fontWeight: "bold" }} for="ten_thuoc">
+              Thêm Thuốc
+            </Label>
+            <FormGroup style={{ display: "flex" }}>
+              <Input
+                type="select"
+                name="ten_thuoc"
+                id="ten_thuoc"
+                onChange={handleMedicineChange}
+                value={selectedMedicineName}
+              >
+                <option value="">Chọn thuốc</option>
+                {medicine.map((item) => (
+                  <option key={item.id} value={item.ten_thuoc}>
+                    {item.ten_thuoc}
+                  </option>
+                ))}
+              </Input>
+              <Input
+                type="number"
+                name="so_luong"
+                id="so_luong"
+                value={quantity}
+                onChange={handleQuantityChange}
+              />
+              <Button color="primary" onClick={handleAddMedicine}>
+                OK
+              </Button>
+            </FormGroup>
           </FormGroup>
 
-          <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
-            {selectedMedicines.map((medicine, index) => (
-              <Row key={index}>
-                <Col>{medicine.ten_thuoc}</Col>
-                <Col>Đơn giá: {medicine.don_gia}</Col>
-                <Col>
-                  <Button
-                    onClick={(event) => removeSelectedMedicine(index, event)}
-                  >
-                    Xóa
-                  </Button>
-                </Col>
-              </Row>
-            ))}
-          </div>
+          {/* Table hiển thị thông tin các thuốc đã chọn */}
+          <table style={{ borderSpacing: "20px" }}>
+            <tbody>
+              {selectedMedicines.map((medicine, index) => (
+                <tr key={index}>
+                  <td style={{ fontWeight: "bold" }}>{medicine.ten_thuoc}</td>
+                  <td>Đơn giá: {medicine.don_gia * 1000}</td>
+                  <td>Số lượng: {medicine.so_luong}</td>
+                  <td>
+                    <Button onClick={() => removeSelectedMedicine(index)}>
+                      Xóa
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
           <FormGroup>
-            <Label for="thoi_gian_tao">Thời gian tạo</Label>
+            <Label style={{ fontWeight: "bold" }} for="xet_nghiem">
+              Thêm Xét Nghiệm
+            </Label>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Input
+                type="select"
+                name="xet_nghiem"
+                id="xet_nghiem"
+                onChange={handleTestChange}
+              >
+                <option value="">Chọn loại xét nghiệm</option>
+                {testData.map((item) => (
+                  <option key={item.id} value={item.ten_xet_nghiem}>
+                    {item.ten_xet_nghiem}
+                  </option>
+                ))}
+              </Input>
+              <Button
+                color="primary"
+                onClick={handleAddTest}
+                style={{ marginLeft: "10px" }}
+              >
+                OK
+              </Button>
+            </div>
+          </FormGroup>
+          <table style={{ borderSpacing: "20px" }}>
+            <tbody>
+              {selectedTests.map((test, index) => (
+                <tr key={index}>
+                  <td style={{ fontWeight: "bold" }}>{test.ten_xet_nghiem}</td>
+                  <td>Đơn giá: {test.gia_tien}</td>
+                  <td>
+                    <Button onClick={() => handleRemoveTest(index)}>Xóa</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <FormGroup>
+            <Label style={{ fontWeight: "bold" }} for="thoi_gian_tao">
+              Thời gian tạo
+            </Label>
             <Input
               type="datetime-local"
               name="thoi_gian_tao"
@@ -221,16 +379,21 @@ function CreateOrderModal({ isOpen, toggleModal, fetchData }) {
             />
           </FormGroup>
           <FormGroup>
-            <Label for="tong_tien">Tổng tiền</Label>
+            <Label style={{ fontWeight: "bold" }} for="tong_tien">
+              Tổng tiền
+            </Label>
             <Input
               type="number"
               name="tong_tien"
               id="tong_tien"
+              value={formData.tong_tien}
               onChange={handleChange}
             />
           </FormGroup>
           <FormGroup>
-            <Label for="phuong_thuc_thanh_toan">Phương Thức thanh toán</Label>
+            <Label style={{ fontWeight: "bold" }} for="phuong_thuc_thanh_toan">
+              Phương Thức thanh toán
+            </Label>
             <Input
               type="text"
               name="phuong_thuc_thanh_toan"
@@ -239,7 +402,9 @@ function CreateOrderModal({ isOpen, toggleModal, fetchData }) {
             />
           </FormGroup>
           <FormGroup>
-            <Label for="ngay_kham">Ngày khám</Label>
+            <Label style={{ fontWeight: "bold" }} for="ngay_kham">
+              Ngày khám
+            </Label>
             <Input
               readOnly
               type="date"
@@ -249,7 +414,9 @@ function CreateOrderModal({ isOpen, toggleModal, fetchData }) {
             />
           </FormGroup>
           <FormGroup>
-            <Label for="gio_kham">Giờ khám</Label>
+            <Label style={{ fontWeight: "bold" }} for="gio_kham">
+              Giờ khám
+            </Label>
             <Input
               readOnly
               type="time"
@@ -259,7 +426,9 @@ function CreateOrderModal({ isOpen, toggleModal, fetchData }) {
             />
           </FormGroup>
           <FormGroup>
-            <Label for="trang_thai">Trạng thái</Label>
+            <Label style={{ fontWeight: "bold" }} for="trang_thai">
+              Trạng thái
+            </Label>
             <Input
               type="text"
               name="trang_thai"
@@ -268,6 +437,11 @@ function CreateOrderModal({ isOpen, toggleModal, fetchData }) {
             />
           </FormGroup>
         </Form>
+        {showSuccessMessage && (
+          <div className="alert alert-success" role="alert">
+            Hóa đơn đã được tạo thành công!
+          </div>
+        )}
       </ModalBody>
       <ModalFooter>
         <Button color="primary" onClick={handleSubmit}>
